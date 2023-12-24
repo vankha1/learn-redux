1d4c21ba613fde9a2a426a344dc9b8f699339b3d
vankha1 <vovankha2003@gmail.com>
2023-12-24 08:02:31 +0700

    dev: add logic for recaptcha and button

--- gameloft-test/src/pages/Agreement/Agreement.jsx
+++ gameloft-test/src/pages/Agreement/Agreement.jsx
@@ -1,18 +1,29 @@
 import ReCAPTCHA from "react-google-recaptcha";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+import { useRef, useState } from 'react'
 
 import Paragraph from "../../components/paragraph/Paragraph";
 import "./agreement.css";
 import paragraphs from "../../mock-paragraph/agreement-para.js";
 
 function Agreement() {
-  console.log(paragraphs);
+    
+  const reCAPTCHA = useRef();
+  const [captchaVerified, setCaptchaVerified] = useState(false);
+
+  const handleCaptchaChange = () => {
+    const captchaValue = reCAPTCHA.current.getValue();
+    if (captchaValue) {
+      setCaptchaVerified(true);
+    }
+  }
+
   return (
     <div className="agreement">
       <div className="agreement-title">
         <h1>Thank you for your interest</h1>
-        <h5>Please read the information below</h5>
-        <hr class="hr-text" data-content="AND"></hr>
+        <hr class="hr-text" data-content="Please read the information below"></hr>
         <p>
           Once you have read and understood the information, if you agree to
           take part in the research, click on the <strong>"I Agree"</strong>{" "}
@@ -32,12 +43,41 @@ function Agreement() {
       </div>
       <div className="agreement-confirm">
         <ReCAPTCHA
-            style={{ marginBottom: "20px"}}
+          ref={reCAPTCHA}
+          style={{ marginBottom: "20px" }}
           sitekey="6LcIlTopAAAAACo40GrelpgzhgSYT5x1dPp03lEO"
+          onChange={handleCaptchaChange}
         />
-        <div style={{marginTop: "10px", display: "flex", gap: "50px"}}>
-            <Button style={{ paddingLeft: "50px", paddingRight: "50px", textTransform: "none" }} size="medium" variant="outlined">Cancel</Button>
-            <Button style={{backgroundColor: "#002248", color: "white", textTransform: "none", paddingLeft: "60px", paddingRight: "60px"}} size="medium" variant="outlined">I have read and agree with the terms of use</Button>
+        <div style={{ marginTop: "10px", display: "flex", gap: "10px" }}>
+          <Button
+            style={{
+              paddingLeft: "65px",
+              paddingRight: "65px",
+              textTransform: "none",
+              color: "#002248",
+              borderColor: "#002248"
+            }}
+            size="medium"
+            variant="outlined"
+          >
+            Cancel
+          </Button>
+          <Button
+            style={{
+              backgroundColor: "#002248",
+              color: "white",
+              textTransform: "none",
+              paddingLeft: "60px",
+              paddingRight: "60px",
+            }}
+            LinkComponent={captchaVerified ? Link : "button"}
+            to={captchaVerified ? "/about" : ""}
+            disabled={!captchaVerified}
+            size="medium"
+            variant="outlined"
+          >
+            I have read and agree with the terms of use
+          </Button>
         </div>
       </div>
     </div>
